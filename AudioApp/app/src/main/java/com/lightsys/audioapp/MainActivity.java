5ed3652ef102aa131579798508122d7584ce8f6a
donaldrshade <donaldrshade@cedarville.edu>
2019-03-05 10:27:45 -0700

    Created about activity
    Created about activity and menu item from the main page.

--- AudioApp/app/src/main/java/com/lightsys/audioapp/MainActivity.java
+++ AudioApp/app/src/main/java/com/lightsys/audioapp/MainActivity.java
@@ -2,23 +2,17 @@ package com.lightsys.audioapp;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import android.content.SharedPreferences;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import java.io.File;
-import android.content.Context;
 
-
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
@@ -118,6 +112,11 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             //Put Action Code here when they select it.
+        }
+        else if(id == R.id.action_about){
+            Intent about = new Intent(this, About.class);
+            startActivity(about);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
