9ecb529d95af52bb44dd745e42f91f2159c0647e
donaldrshade <donaldrshade@cedarville.edu>
2019-03-05 10:48:04 -0700

    Added Menu to Lesson
    Added the menu to the lesson activity to toggle note taking

--- AudioApp/app/src/main/java/com/lightsys/audioapp/lessonActivity.java
+++ AudioApp/app/src/main/java/com/lightsys/audioapp/lessonActivity.java
@@ -6,6 +6,8 @@ import android.nfc.FormatException;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import android.media.MediaPlayer;
@@ -172,5 +174,28 @@ public class lessonActivity extends AppCompatActivity {
         mContentControlsView.setVisibility(View.VISIBLE);
         mMediaControlsView.setVisibility(View.VISIBLE);
         mVisible = true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_lesson, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_notes) {
+            //add code to do the switching here.
+            Toast.makeText(this, "Take Some Good Notes", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
