cc768339cae0e5f3df12f496162f86c74f1330bb
Galhanone <d.dan.gar.g@gmail.com>
2019-03-05 12:39:34 -0500

    MP3 files can now be located from a string

--- AudioApp/app/src/main/java/com/lightsys/audioapp/lessonActivity.java
+++ AudioApp/app/src/main/java/com/lightsys/audioapp/lessonActivity.java
@@ -2,20 +2,15 @@ package com.lightsys.audioapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.media.AudioManager;
+import android.nfc.FormatException;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
 
 import android.media.MediaPlayer;
 import android.widget.ImageButton;
-
-import java.io.IOException;
+import android.widget.Toast;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -23,19 +18,22 @@ import java.io.IOException;
  */
 public class lessonActivity extends AppCompatActivity {
 
-    private View mContentView;
-    private View mContentControlsView;
-    private View mMediaControlsView;
+    private View mContentView;          //Main page
+    private View mContentControlsView;  //Bottom controls
+    private View mMediaControlsView;    //Top (audio) controls
 
     private boolean mVisible;
 
-
+    //Media Buttons
     private MediaPlayer media = null;
     private ImageButton play;
     private ImageButton back10;
     private ImageButton fwd10;
     private ImageButton prev;
     private ImageButton next;
+
+    //Content Buttons
+    private FloatingActionButton home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +46,20 @@ public class lessonActivity extends AppCompatActivity {
         mMediaControlsView = findViewById(R.id.media_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
-        String uri = "l1_born_again.mp3";
-        media = createMedia(uri);
+        //TODO: Get string from Script file thing
+        String mp3 = "l1_born_again.mp3";
+        try {
+            media = createMedia(mp3);
+        } catch (FormatException e) {
+            Toast.makeText(getApplicationContext(), "Incorrect file format", Toast.LENGTH_SHORT).show();
+            mainActivity();
+        }
 
+        //If audio is playing, clicking play button pauses it
+        //Otherwise, play audio
         play = findViewById(R.id.play_button);
         play.setImageDrawable(getResources().getDrawable(R.drawable.ic_play));
         play.setOnClickListener(new View.OnClickListener() {
-            //If audio is playing, clicking this pauses it
-            //Otherwise, play audio
             @Override
             public void onClick(View view) {
                 if(media.isPlaying()){
@@ -68,47 +72,49 @@ public class lessonActivity extends AppCompatActivity {
             }
         });
 
+        //Seek to 10 seconds behind current position
         back10 = findViewById(R.id.back10_button);
         back10.setImageDrawable(getResources().getDrawable(R.drawable.ic_back10));
         back10.setOnClickListener(new View.OnClickListener() {
-            //Seek to 10 seconds behind current position
             @Override
             public void onClick(View view) {
                 media.seekTo(media.getCurrentPosition() - 10000);
             }
         });
 
+        //Seek to 10 seconds ahead of current position
         fwd10 = findViewById(R.id.fwd10_button);
         fwd10.setImageDrawable(getResources().getDrawable(R.drawable.ic_fwd10));
         fwd10.setOnClickListener(new View.OnClickListener() {
-            //Seek to 10 seconds ahead of current position
             @Override
             public void onClick(View view) {
                 media.seekTo(media.getCurrentPosition() + 10000);
             }
         });
 
+        //Seek back to start of audio
         prev = findViewById(R.id.previous_button);
         prev.setImageDrawable(getResources().getDrawable(R.drawable.ic_prev));
         prev.setOnClickListener(new View.OnClickListener() {
-            //Seek back to start of audio
             @Override
             public void onClick(View view) {
                 media.seekTo(0);
             }
         });
 
+        //Seek to end of audio
+        //TODO: When end of audio reached, prompt to load next lesson
         next = findViewById(R.id.next_button);
         next.setImageDrawable(getResources().getDrawable(R.drawable.ic_next));
         next.setOnClickListener(new View.OnClickListener() {
-            //Seek to end of audio
             @Override
             public void onClick(View view) {
                 media.seekTo(media.getDuration());
             }
         });
 
-        FloatingActionButton home = findViewById(R.id.home_button);
+        //TODO: Put this in menu rather than FAB
+        home = findViewById(R.id.home_button);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -119,40 +125,47 @@ public class lessonActivity extends AppCompatActivity {
             }
         });
 
-        // Set up the user interaction to manually show or hide the system UI.
+        //When activity screen clicked, toggle visibility of controls
         mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toggle();
+                if (mVisible) {
+                    hide();
+                } else {
+                    show();
+                }
             }
         });
     }
 
-    private MediaPlayer createMedia(String url){
-        MediaPlayer mediaPlayer = MediaPlayer.create(lessonActivity.this, R.raw.l1_born_again);
-        return mediaPlayer;
+    //Creates and prepares media to be played
+    //Throws FormatException if file is not an mp3
+    private MediaPlayer createMedia(String mp3) throws FormatException {
+        if(mp3.contains(".mp3")) {
+            String file = mp3.replace(".mp3", "");
+            int id = getResources().getIdentifier(file,"raw", getPackageName());
+            MediaPlayer mediaPlayer = MediaPlayer.create(lessonActivity.this, id);
+            return mediaPlayer;
+        } else {
+            throw new FormatException();
+        }
     }
 
+    //Returns from lessonActivity to MainActivity
     private void mainActivity() {
         Intent main = new Intent(lessonActivity.this, MainActivity.class);
         startActivity(main);
         finish();
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
+    //Hide controls
     private void hide() {
         mContentControlsView.setVisibility(View.GONE);
         mMediaControlsView.setVisibility(View.GONE);
         mVisible = false;
     }
 
+    //Show controls
     @SuppressLint("InlinedApi")
     private void show() {
         // Show the system bar
