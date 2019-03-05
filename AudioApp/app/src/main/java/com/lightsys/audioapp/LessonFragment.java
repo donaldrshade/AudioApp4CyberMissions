f5fc35b4f71ee9569d82da6b0fbf8d57cbf4a148
Gaden32 <48032360+Gaden32@users.noreply.github.com>
2019-03-05 18:50:47 -0500

    Commit

---
+++ AudioApp/app/src/main/java/com/lightsys/audioapp/LessonFragment.java
@@ -0,0 +1,6 @@
+package com.lightsys.audioapp;
+
+import android.support.v4.app.Fragment;
+
+public class LessonFragment extends Fragment {
+}
--- AudioApp/app/src/main/java/com/lightsys/audioapp/Recycler_View_Adapter.java
+++ AudioApp/app/src/main/java/com/lightsys/audioapp/Recycler_View_Adapter.java
@@ -3,6 +3,7 @@ package com.lightsys.audioapp;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerViewAccessibilityDelegate;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,32 +11,23 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
 import java.util.ArrayList;
 
-
-/*  class Recycler_View_Adapter
-        Creating a Recycling list with the size of "courseName"
-        using the layout from "layout_course_listitem.xml"
- */
 public class Recycler_View_Adapter extends RecyclerView.Adapter<Recycler_View_Adapter.ViewHolder>{
     private static final String TAG = "Recycler_View_Adapter";
 
-    //The names of the courses to be displayed
-    private ArrayList<String> mcourseNames = new ArrayList<>();
-
+    private ArrayList<String> mTextNames = new ArrayList<>();
     private Context mContext;
 
-    //Default constructor
-    public Recycler_View_Adapter(ArrayList<Course> courseName, Context context){
+    public Recycler_View_Adapter(ArrayList<String> textName, Context context){
         mContext = context;
-        for (int i = 0; i < courseName.size(); i++){
-            mcourseNames.add(courseName.get(i).name);
-        }
+        mTextNames = textName;
     }
 
     @NonNull
     @Override
-    //Recycling the view holders
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_course_listitem, viewGroup, false);
         ViewHolder holder = new ViewHolder(view);
@@ -44,22 +36,16 @@ public class Recycler_View_Adapter extends RecyclerView.Adapter<Recycler_View_Ad
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int position) {
-        Log.d(TAG, "onBindViewHolder: called."); //Log for debugging
+        Log.d(TAG, "onBindViewHolder: called.");
 
-        viewHolder.courseName.setText(mcourseNames.get(position));
+        viewHolder.textName.setText(mTextNames.get(position));
+
         viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
-
-            /**
-             *  This function is called when a list item is clicked/tapped.
-             *  More functionality should be added to this function
-             */
             @Override
             public void onClick(View v) {
-                //Logging for debugging
-                Log.d(TAG, "onClick: Clicked on : " + mcourseNames.get(position));
-                Toast.makeText(mContext, mcourseNames.get(position), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "onClick: Clicked on : " + mTextNames.get(position));
 
-
+                Toast.makeText(mContext, mTextNames.get(position), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -67,24 +53,16 @@ public class Recycler_View_Adapter extends RecyclerView.Adapter<Recycler_View_Ad
 
     @Override
     public int getItemCount() {
-        return mcourseNames.size();
+        return mTextNames.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-        /** Variables */
-
-        //Course name
-        TextView courseName;
-
-        //The layout for each list item
+        TextView textName;
         RelativeLayout parentLayout;
-
-
-        /** Constructor */
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            courseName = itemView.findViewById(R.id.course_name);
+            textName = itemView.findViewById(R.id.text_name);
             parentLayout = itemView.findViewById(R.id.parent_layout);
         }
     }
