4cb8fef30938f30b4ae72f64d2b0b6359b9571db
Galhanone <d.dan.gar.g@gmail.com>
2019-03-05 15:05:33 -0700

    Update MainActivity.java

--- AudioApp/app/src/main/java/com/lightsys/audioapp/MainActivity.java
+++ AudioApp/app/src/main/java/com/lightsys/audioapp/MainActivity.java
@@ -5,17 +5,21 @@ import java.util.ArrayList;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
 import java.io.File;
 
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
     File fileDir;
     ArrayList Courses;
     @Override
@@ -39,15 +43,17 @@ public class MainActivity extends AppCompatActivity {
         */
         getCourseData();
         initRecyclerView();
-    }
 
-       FloatingActionButton fab = findViewById(R.id.fab);
+        //TODO:Remove this once RecyclerView is implemented
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 lessonActivity();
             }
-        }
+        });
+    }
+
     private void getCourseData() {
         Courses = new ArrayList<Course>();//init the Array List
 
